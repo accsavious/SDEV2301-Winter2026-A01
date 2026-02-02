693cfd827a02f6604acd189f43e53786a57ddc23
Jared <jkarpiak@nait.ca>
2026-02-02 07:09:45 -0700

    Added Advanced LINQ Lesson

---
+++ Lesson 12 - Advanced LINQ/Exercises/LinqBookStore_End/BookStoreTests/LinqTests.cs
@@ -0,0 +1,76 @@
+﻿using LinqBookStore;
+
+namespace BookStoreTests
+{
+    public class LinqTests
+    {
+        [Fact]
+        public void GetBooksByPrice_MiddlePrice_ReturnsCorrectResults()
+        {
+            List<Book> data = Program.PopulateData();
+            IEnumerable<Book> filteredBooks = Program.GetBooksByPrice(data, 9.99);
+            Assert.Equal(9, filteredBooks.Count());
+        }
+        [Fact]
+        public void GetGenres_All_ReturnsAllGenres()
+        {
+            List<Book> data = Program.PopulateData();
+            IEnumerable<string> genres = Program.GetGenres(data);
+            Assert.Equal(8, genres.Count());
+        }
+        [Fact]
+        public void GetBooksByGenre_ClassicGenre_ReturnsCorrectResults()
+        {
+            List<Book> data = Program.PopulateData();
+            IEnumerable<Book> books = Program.GetBooksByGenre(data, "Classic").ToList();
+            Assert.Equal("Pride and Prejudice", books.FirstOrDefault().Title);
+        }
+        [Fact]
+        public void GetBooksByPublishDate_2000_01_01_ReturnsCorrectResults()
+        {
+            List<Book> data = Program.PopulateData();
+            DateTime.TryParse("2000-01-01", out DateTime date);
+            IEnumerable<Book> books = Program.GetBooksByPublishDate(data, date);
+            Assert.Equal(4, books.Count());
+        }
+        [Fact]
+        public void GetBooksByRating_Rating8_ReturnsCorrectResults()
+        {
+            List<Book> data = Program.PopulateData();
+            IEnumerable<Book> books = Program.GetBooksByRating(data, 8).ToList();
+            Assert.Equal("Dune", books.FirstOrDefault().Title);
+        }
+        [Theory]
+        [InlineData("Fantasy", 6)]
+        [InlineData("Mystery", 1)]
+        public void GetNumberOfBooksByGenre_MultipleGenres_ReturnsCorrectCount(string genre, int expectedCount)
+        {
+            List<Book> data = Program.PopulateData();
+            int numBooks = Program.GetNumberOfBooksByGenre(data, genre);
+            Assert.Equal(expectedCount, numBooks);
+        }
+        [Fact]
+        public void GetTotalValueOfBooks_CompleteInventory_ReturnsCorrectSum()
+        {
+            List<Book> data = Program.PopulateData();
+            double expectedValue = 183.28;
+            double totalValue = Program.GetTotalValueOfBooks(data);
+
+            Assert.Equal(expectedValue, Math.Round(totalValue, 2));
+        }
+        [Fact]
+        public void GetCheapestBooks_LowestPrice_ReturnsCorrectResults()
+        {
+            List<Book> data = Program.PopulateData();
+            IEnumerable<Book> results = Program.GetCheapestBooks(data);
+            Assert.Equal(1, results.Count());
+        }
+        [Fact]
+        public void GetCheapestBooks_HighestPrice_ReturnsCorrectResults()
+        {
+            List<Book> data = Program.PopulateData();
+            IEnumerable<Book> results = Program.GetMostExpensiveBooks(data);
+            Assert.Equal(4, results.Count());
+        }
+    }
+}
---
+++ Lesson 12 - Advanced LINQ/Exercises/LinqBookStore_End/LinqBookStore/Book.cs
@@ -0,0 +1,42 @@
+﻿namespace LinqBookStore
+{
+    public class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Author(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int Rating { get; set; }
+        public double Price { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Genre { get; set; }
+
+
+        public Book(string title, string authorFirst, string authorSecond, DateTime publishDate, int rating, double price, int numberOfPages, string genre)
+        {
+            Title = title;
+            Author = new Author(authorFirst, authorSecond);
+            PublishDate = publishDate;
+            Rating = rating;
+            Price = price;
+            NumberOfPages = numberOfPages;
+            Genre = genre;
+        }
+
+    }
+}
