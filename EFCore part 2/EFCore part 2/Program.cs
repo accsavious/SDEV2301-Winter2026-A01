0eaa4844c7009e5fb69f8e9cd9bc489bdecc374d
Sean Paulson <auggle@hotmail.com>
2026-02-09 23:11:20 -0700

    EFCore part 2

---
+++ EFCore part 2/EFCore part 2/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EFCore_part_2
+{
+    public class Course
+
+    {
+
+        public int Id { get; set; }
+
+        public string Code { get; set; } = "";
+
+        public string Name { get; set; } = "";
+
+        public int Credits { get; set; } = 3;
+        // Collection navigation property
+        public List<Student> Students { get; set; } = new();
+    }
+
+    public class Student
+
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; }
+        // Reference navigation property
+        public Course? Course { get; set; }
+        // Foreign Key
+        public int CourseId { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Course> Courses => Set<Course>();
+        public DbSet<Student> Students => Set<Student>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "school.db"
+
+            );
+            dbPath = Path.GetFullPath(dbPath);
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(b =>
+            {
+                b.Property(s => s.Name).IsRequired().HasMaxLength(100);
+                
+            });
+            modelBuilder.Entity<Course>(b =>
+            {
+                b.Property(c => c.Name).IsRequired().HasMaxLength(100);
+                b.Property(c => c.Code).IsRequired().HasMaxLength(20);
+                b.HasMany(c => c.Students)
+                .WithOne(s => s.Course!)
+                .HasForeignKey(s => s.CourseId)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SchoolContext();
+            context.Database.Migrate();
+            SchoolService service = new SchoolService();
+            service.CreateExample(context);
+            service.UpdateExample(context);
+            service.ReadExample(context);
+            service.DeleteExample(context);
+        }
+    }
+}
---
+++ EFCore part 2/EFCore part 2/SchoolService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_part_2
+{
+    internal class SchoolService
+    {
+        public Course GetSafeCourse(SchoolContext context, string courseCode)
+        {
+            var loadedCourse = context.Courses
+                .Include(c => c.Students)
+                .Single(c => c.Code == "RING0000");
+            return loadedCourse;
+        }
+
+        public void CreateExample(SchoolContext context)
+        {
+            if (!context.Courses.Any() && !context.Students.Any())
+            {
+                var course = new Course { Name = "Ringsmithing Fundamentals", Code = "RING0000", Credits = 3 };
+                context.Courses.Add(course);
+
+                context.Students.AddRange(
+                    new Student { Name = "Sauron", Age = 3232, Course = course },
+                    new Student { Name = "Saruman", Age = 400, Course = course },
+                    new Student { Name = "Frodo", Age = 50, Course = course }
+                    );
+                context.SaveChanges();
+            }
+        }
+
+        public void ReadExample(SchoolContext context)
+        {
+            var loadedCourse = context.Courses
+                .Include(c => c.Students)
+                .Single(c => c.Code == "RING0000");
+
+            Console.WriteLine($"\nREAD: {loadedCourse.Code} - {loadedCourse.Name} ({loadedCourse.Credits} credits)");
+            foreach (var s in loadedCourse.Students)
+                Console.WriteLine($" - Student #{s.Id}: {s.Name} (Age {s.Age})");
+        }
+
+        public void UpdateExample(SchoolContext context)
+        {
+            var loadedCourse = context.Courses
+                .Include(c => c.Students)
+                .Single(c => c.Code == "RING0000");
+
+            loadedCourse.Credits = 5;
+            if (loadedCourse.Students.Where(s => s.Name == "Frodo").Count() > 0)
+            {
+                var frodo = loadedCourse.Students.Single(s => s.Name == "Frodo");
+                frodo.Age = 200;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteExample(SchoolContext context)
+        {
+            var loadedCourse = context.Courses
+                .Include(c => c.Students)
+                .Single(c => c.Code == "RING0000");
+            if (loadedCourse.Students.Where(s => s.Name == "Frodo").Count() > 0)
+            {
+                var frodo = loadedCourse.Students.Single(s => s.Name == "Frodo");
+                context.Students.Remove(frodo);
+            }
+
+            context.Courses.Remove(loadedCourse);
+            context.SaveChanges();
+        }
+
+
+    }
+}
