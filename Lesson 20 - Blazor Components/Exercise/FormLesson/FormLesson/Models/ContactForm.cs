5f688b24970be8cec13b0a960db2d40125dc3cbe
Sean Paulson <auggle@hotmail.com>
2026-02-26 13:53:34 -0700

    learned form validation

---
+++ Lesson 20 - Blazor Components/Exercise/FormLesson/FormLesson/Models/ContactForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormLesson.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; } = "";
+        [Range(18, 130)]
+        public int Age { get; set; }
+    }
+}
