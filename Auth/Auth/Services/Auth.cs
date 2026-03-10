e7029ddd1c049c3dba2f8439d1aa55ab54ddd6b7
Sean Paulson <auggle@hotmail.com>
2026-03-10 16:58:38 -0600

    learned auth

---
+++ Auth/Auth/Services/Auth.cs
@@ -0,0 +1,21 @@
+﻿namespace Auth.Services
+{
+    public class AuthService
+    {
+        public string? CurrentUser { get; private set; }
+
+        public bool IsLoggedIn => !string.IsNullOrEmpty(CurrentUser);
+
+        public bool Login(string username, string password)
+        {
+            if (username == "user2301" && password == "Password2301")
+            {
+                CurrentUser = username;
+                return true;
+            }
+            return false;
+        }
+
+        public void Logout() => CurrentUser = null;
+    }
+}
