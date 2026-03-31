5f05bbc15e2d00c3e5fb05434ef0a539afaab5bc
Sean Paulson <auggle@hotmail.com>
2026-03-31 11:29:27 -0600

    learned maui layout and bridges

---
+++ MauiLayout/MauiLayout/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MauiLayout.Models;
+
+namespace MauiLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlertAsync(
+                "Confirm Action",
+                "Do you want to continue?",
+                "Yes",
+                "No");
+
+            ConfirmationBridge.Instance.PublishResult(answer);
+        }
+    }
+}
---
+++ MauiLayout/MauiLayout/Models/ConfirmationBridge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiLayout.Models
+{
+    internal class ConfirmationBridge
+    {
+        public static ConfirmationBridge Instance { get; }
+            = new ConfirmationBridge();
+
+        public event Action<bool> ConfirmationChanged;
+
+        public void PublishResult(bool result) {
+            ConfirmationChanged?.Invoke(result);
+        }
+    }
+}
